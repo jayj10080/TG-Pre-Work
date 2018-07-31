633954ae5306d900ac838cfeafc9f4c161144146
Jay Johnson <jayj10080@gmail.com>
2018-07-31 17:42:39 -0600

    7. String Interpolation 2

--- javascript.js
+++ javascript.js
@@ -1,2 +1,3 @@
-var favoriteAnimal = "Cat";
-console.log("My favorite animal: " + favoriteAnimal);
+var myName = "Jay";
+var myCity = "Boulder";
+console.log(`My name is ${myName}. My favorite city is ${myCity}.`)
