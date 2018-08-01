7305b1db18f4f2de6e9e6d07175c4e55ba25dfaa
Jay Johnson <jayj10080@gmail.com>
2018-08-01 17:58:44 -0600

    3. Global Scope 2

--- javascript.js
+++ javascript.js
@@ -3,7 +3,9 @@ const galaxy = 'The Milky Way';
 let stars = 'North Star';
 
 const myNightSky = () => {
+  stars = 'Sirius';
   return 'Night Sky: ' + satellite + ', ' + stars + ', ' + galaxy;
 }
 
 console.log(myNightSky());
+console.log(stars);
