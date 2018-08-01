d051fae74f727f624abcb97ffcab6429e04ed92a
Jay Johnson <jayj10080@gmail.com>
2018-08-01 17:44:59 -0600

    6. Return 2

--- javascript.js
+++ javascript.js
@@ -1,14 +1,26 @@
-var orderCount = 0;
+let orderCount = 0;
+
 const takeOrder = (topping, crustType) => {
   orderCount++;
   console.log('Order: ' + crustType + ' pizza topped with ' + topping);
-  console.log(getSubTotal(orderCount));
 };
-
-const getSubTotal = (itemCount) => {
-  return itemCount * 7.5;
-}
 
 takeOrder('mushroom', 'thin crust');
 takeOrder('spinach', 'whole wheat');
 takeOrder('pepperoni', 'brooklyn style');
+
+const getSubTotal = (itemCount) => {
+  return itemCount * 7.5;
+};
+
+const getTax = (itemCount) => {
+  return getSubTotal(itemCount) * 0.06;
+}
+
+const getTotal = (itemCount) => {
+  return getSubTotal(itemCount) + getTax(itemCount);
+}
+
+console.log(getTotal(orderCount));
+
+console.log(getTotal(orderCount));
