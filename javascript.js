11e82bab1bb58113c07fd1de4191da6c15b385a6
Jay Johnson <jayj10080@gmail.com>
2018-08-04 08:53:34 -0600

    7. More Array Methods

--- javascript.js
+++ javascript.js
@@ -1,12 +1,7 @@
-var newYearsResolutions = ["Eat", "Drink", "Sleep"];
-console.log(newYearsResolutions);
-var listItem = newYearsResolutions[0];
-console.log(listItem);
-console.log(newYearsResolutions[2]);
-console.log(newYearsResolutions[3]);
-newYearsResolutions[1] = "Learn a new language"
-console.log(newYearsResolutions.length);
-newYearsResolutions.push("Talk", "Think");
-console.log(newYearsResolutions);
-newYearsResolutions.pop();
-console.log(newYearsResolutions);
+let groceryList = ['orange juice', 'bananas', 'coffee beans', 'brown rice', 'pasta', 'coconut oil', 'plantains'];
+groceryList.shift();
+console.log(groceryList);
+groceryList.unshift("popcorn");
+console.log(groceryList);
+console.log(groceryList.slice(1, 4));
+console.log(groceryList);
