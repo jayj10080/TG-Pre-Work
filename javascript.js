34550246d5f4bbf040e01c3e6fca955d3e61a64b
Jay Johnson <jayj10080@gmail.com>
2018-08-04 08:46:52 -0600

    5. length property

--- javascript.js
+++ javascript.js
@@ -5,3 +5,4 @@ console.log(listItem);
 console.log(newYearsResolutions[2]);
 console.log(newYearsResolutions[3]);
 newYearsResolutions[1] = "Learn a new language"
+console.log(newYearsResolutions.length);
