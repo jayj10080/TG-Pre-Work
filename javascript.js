7bdd70cbf16029cc3984957eca3d8e10b15d9c59
Jay Johnson <jayj10080@gmail.com>
2018-08-04 08:43:32 -0600

    4. Update Elements

--- javascript.js
+++ javascript.js
@@ -4,3 +4,4 @@ var listItem = newYearsResolutions[0];
 console.log(listItem);
 console.log(newYearsResolutions[2]);
 console.log(newYearsResolutions[3]);
+newYearsResolutions[1] = "Learn a new language"
