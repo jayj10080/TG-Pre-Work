e2f4dc1a961f3d49b01fe54e5885d2930c4649f8
Jay Johnson <jayj10080@gmail.com>
2018-08-04 08:42:29 -0600

    3. Property Access

--- javascript.js
+++ javascript.js
@@ -1,2 +1,6 @@
 var newYearsResolutions = ["Eat", "Drink", "Sleep"];
 console.log(newYearsResolutions);
+var listItem = newYearsResolutions[0];
+console.log(listItem);
+console.log(newYearsResolutions[2]);
+console.log(newYearsResolutions[3]);
