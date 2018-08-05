6a61a7b9ddc150780df9e205893565d53718765b
Jay Johnson <jayj10080@gmail.com>
2018-08-05 13:06:32 -0600

    3. .map()

--- javascript.js
+++ javascript.js
@@ -1,4 +1,15 @@
-let fruits = ['mango', 'papaya', 'pineapple', 'apple'];
+let animals = ['Hen', 'elephant', 'llama', 'leopard', 'ostrich', 'Whale', 'octopus', 'rabbit', 'lion', 'dog'];
 
-fruits.forEach(item =>
-console.log('I want to eat a ' + item));
+// Create the secretMessage array below
+let secretMessage = animals.map(item => 
+  item[0]);
+
+
+console.log(secretMessage.join(''));
+
+let bigNumbers = [100, 200, 300, 400, 500];
+
+// Create the smallNumbers array below
+let smallNumbers = bigNumbers.map(function(item) {
+  return item/100;
+})
