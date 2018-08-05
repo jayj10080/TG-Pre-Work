c664ce9c665f3a23bc87dd3ce389e8a4d63ab18f
Jay Johnson <jayj10080@gmail.com>
2018-08-05 12:56:26 -0600

    1. Introduction to Iterators

--- javascript.js
+++ javascript.js
@@ -1,8 +1,21 @@
-let groceryList = ['orange juice', 'bananas', 'coffee beans', 'brown rice', 'pasta', 'coconut oil', 'plantains'];
-groceryList.shift();
-console.log(groceryList);
-groceryList.unshift("popcorn");
-console.log(groceryList);
-console.log(groceryList.slice(1, 4));
-console.log(groceryList);
+let artists = ['Picasso', 'Kahlo', 'Matisse', 'Utamaro'];
 
+artists.forEach(function(artist) {
+  console.log(artist + ' is one of my favorite artists.');
+});
+
+let numbers = [1, 2, 3, 4, 5];
+
+let squareNumbers = numbers.map(function(number) {
+  return number * number;
+});
+
+console.log(squareNumbers);
+
+let things = ['desk', 'chair', 5, 'backpack', 3.14, 100];
+
+let onlyNumbers = things.filter(function(thing) {
+  return typeof thing === 'number';
+});
+
+console.log(onlyNumbers);
