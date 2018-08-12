39fb6e0c70be866475f1a93e789564897a1cb7d6
Jay Johnson <jayj10080@gmail.com>
2018-08-12 13:54:51 -0600

    11. Export as

--- airplane.js
+++ airplane.js
@@ -1,23 +1,24 @@
-export let availableAirplanes = [{
- name: 'AeroJet',
-  maxSpeed: 1200,
-  minSpeed: 300,
+let availableAirplanes = [
+{name: 'AeroJet',
  fuelCapacity: 800,
  availableStaff: ['pilots', 'flightAttendants', 'engineers', 'medicalAssistance', 'sensorOperators'],
+ maxSpeed: 1200,
+ minSpeed: 300
 }, 
 {name: 'SkyJet',
- maxSpeed: 800,
- minSpeed: 200,
  fuelCapacity: 500,
- availableStaff: ['pilots', 'flightAttendants']
-}];
+ availableStaff: ['pilots', 'flightAttendants'],
+ maxSpeed: 800,
+ minSpeed: 200
+}
+];
 
-export let flightRequirements = {
+let flightRequirements = {
   requiredStaff: 4,
   requiredSpeedRange: 700
 };
 
-export function meetsStaffRequirements(availableStaff, requiredStaff) {
+function meetsStaffRequirements(availableStaff, requiredStaff) {
   if (availableStaff.length >= requiredStaff) {
     return true;
   } else {
@@ -25,11 +26,13 @@ export function meetsStaffRequirements(availableStaff, requiredStaff) {
   }
 };
 
-export function meetsSpeedRangeRequirements(maxSpeed, minSpeed, requiredSpeedRange) {
-  var range = maxSpeed - minSpeed;
+function meetsSpeedRangeRequirements(maxSpeed, minSpeed, requiredSpeedRange) {
+  let range = maxSpeed - minSpeed;
   if (range > requiredSpeedRange) {
     return true;
-  } else {
+    } else {
     return false;
   }
-}
+};
+
+export { availableAirplanes as aircrafts, flightRequirements as flightReqs, meetsStaffRequirements as meetsStaffReqs, meetsSpeedRangeRequirements as meetsSpeedRangeReqs };
