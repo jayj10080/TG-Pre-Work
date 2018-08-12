0658194e1d06e833b9dbbccdcdae1d9ba0047035
Jay Johnson <jayj10080@gmail.com>
2018-08-12 13:42:45 -0600

    9. Export Named Exports

--- airplane.js
+++ airplane.js
@@ -1,18 +1,23 @@
-let availableAirplanes = [{
+export let availableAirplanes = [{
  name: 'AeroJet',
+  maxSpeed: 1200,
+  minSpeed: 300,
  fuelCapacity: 800,
  availableStaff: ['pilots', 'flightAttendants', 'engineers', 'medicalAssistance', 'sensorOperators'],
 }, 
 {name: 'SkyJet',
+ maxSpeed: 800,
+ minSpeed: 200,
  fuelCapacity: 500,
  availableStaff: ['pilots', 'flightAttendants']
 }];
 
-let flightRequirements = {
+export let flightRequirements = {
   requiredStaff: 4,
+  requiredSpeedRange: 700
 };
 
-function meetsStaffRequirements(availableStaff, requiredStaff) {
+export function meetsStaffRequirements(availableStaff, requiredStaff) {
   if (availableStaff.length >= requiredStaff) {
     return true;
   } else {
@@ -20,4 +25,11 @@ function meetsStaffRequirements(availableStaff, requiredStaff) {
   }
 };
 
-export { availableAirplanes, flightRequirements, meetsStaffRequirements};
+export function meetsSpeedRangeRequirements(maxSpeed, minSpeed, requiredSpeedRange) {
+  var range = maxSpeed - minSpeed;
+  if (range > requiredSpeedRange) {
+    return true;
+  } else {
+    return false;
+  }
+}
