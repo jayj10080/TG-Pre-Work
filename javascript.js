5fac95dd50ec066b690f8716b1646ee54aac849b
Jay Johnson <jayj10080@gmail.com>
2018-08-11 18:05:56 -0600

    8. Inheritance 3

--- javascript.js
+++ javascript.js
@@ -13,8 +13,18 @@ class HospitalEmployee {
   }
   
   takeVacationDays(daysOff) {
-    this._remainingVacationDays = this._remainingVacationDays - daysOff;
+    this._remainingVacationDays -= daysOff;
   }
 }
+
+class Nurse extends HospitalEmployee {
+  constructor(name, certifications) {
+    super(name);
+    this._remainingVacationDays = 20;
+    this._certifications = certifications;
+  }
+}
+
+const nurseOlynyk = new Nurse('Olynyk', ['Trauma', 'Pediatrics']);
 
 
