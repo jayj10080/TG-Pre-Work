65e059eb15c4c1430d917465d3454da1d1fee840
Jay Johnson <jayj10080@gmail.com>
2018-08-12 19:12:48 -0600

    9. fetch() POST Requests 4

--- javascript.js
+++ javascript.js
@@ -10,11 +10,23 @@ const responseField = document.querySelector('#responseField');
 // AJAX functions
 const shortenUrl = () => {
   const urlToShorten = inputField.value;
-  const data = JSON.stringify({destination: urlToShorten});
-  fetch(url, {method: 'POST', headers: {
-  'Content-type': 'application/json',
-  'apikey': apiKey
-}, body: data});
+  const data = JSON.stringify({destination: urlToShorten})
+  
+  fetch(url, {
+    method: 'POST',
+    headers: {
+      'Content-type': 'application/json',
+      'apikey': apiKey
+    },
+    body: data
+  }).then(response => {
+    if (response.ok) {
+      return response.json();
+    }
+    throw new Error('Request failed!');
+  }, networkError => {
+    console.log(networkError.message)
+  })
 }
 
 // Clear page and call AJAX functions
