fcf210a4bf68a24fc252abe9ce503ed694b5a757
Jay Johnson <jayj10080@gmail.com>
2018-08-12 19:29:48 -0600

    16. async POST Requests 3

--- javascript.js
+++ javascript.js
@@ -1,15 +1,40 @@
-const getData = async () => {
+const apiKey = '887c30c7105d453e9230622db139642f';
+const url = 'https://api.rebrandly.com/v1/links';
+
+
+const inputField = document.querySelector('#input');
+const shortenButton = document.querySelector('#shorten');
+const responseField = document.querySelector('#responseField');
+
+const shortenUrl = async () => {
+  const urlToShorten = inputField.value;
+  const data = JSON.stringify({destination: urlToShorten});
   try {
-    const response = await fetch('https://api-to-call.com/endpoint', {
+    const response = await fetch(url, {
       method: 'POST',
-      body: JSON.stringify({id: 200})
-    })
+      body: data,
+      headers: {
+        'Content-type': 'application/json',
+        'apikey': apiKey
+      }
+    });
     if(response.ok){
       const jsonResponse = await response.json();
-      return jsonResponse;
+      renderResponse(jsonResponse);
     }
-    throw new Error('Request failed!');
-  } catch(error) {
+  } catch (error) {
     console.log(error);
   }
 }
+
+
+const displayShortUrl = (event) => {
+  event.preventDefault();
+  while(responseField.firstChild){
+    responseField.removeChild(responseField.firstChild);
+  }
+  shortenUrl();
+}
+
+shortenButton.addEventListener('click', displayShortUrl);
+
